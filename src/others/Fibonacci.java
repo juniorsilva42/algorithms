f55f1033aa991316253dbc434e807e4561d495bf
jsiilva1 <jsiilva@outlook.com.br>
2018-12-20 02:59:48 -0200

    Documentado o método recursivo da série de fibonacci

--- src/others/Fibonacci.java
+++ src/others/Fibonacci.java
@@ -4,14 +4,15 @@ public class Fibonacci {
 
     public static void main (String... args) {
 
-        System.out.println(fibonacci(10));
+        for (int i = 1; i <= 10; i++)
+            System.out.print(fibonacci(i) + " - ");
     }
 
     /**
      * Método recursivo que executa a série de fibonacci em um dado numero
      *
      * @param n número alvo para obter a série de fibonacci
-     * @return o número de fibonnaci de n
+     * @return a série de fibonnaci de n
      */
     private static int fibonacci (int n) {
         if (n == 1 ||  n == 2)
