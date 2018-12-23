96b402497e49a3807d85e1eee4e698d2bf61f046
jsiilva1 <jsiilva@outlook.com.br>
2018-12-22 22:08:26 -0200

    Implementando a classe RSA do pacote \ciphers

--- src/ciphers/RSA.java
+++ src/ciphers/RSA.java
@@ -1,4 +1,18 @@
 package ciphers;
 
+import java.math.BigInteger;
+
 public class RSA {
+
+    private BigInteger pubkey, privKey, modulus;
+
+    public RSA (int bits) { }
+
+    public synchronized String encrypt (String string) {
+        return (new BigInteger(string.getBytes())).modPow(pubkey, modulus).toString();
+    }
+
+    public synchronized BigInteger encrypt (BigInteger string) {
+        return string.modPow(pubkey, modulus);
+    }
 }
